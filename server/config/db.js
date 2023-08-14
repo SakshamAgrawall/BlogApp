765c0b0d0fce49be18f7b5669c3cd60f00eac917
saksham <sakshamagrawal18@gmail.com>
2023-08-14 17:10:52 +0530

    blog app

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+// mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected to Mongodb Database `
+    );
+  } catch (error) {
+    console.log(`Connection Error ${error}`);
+  }
+};
+
+module.exports = connectDB;
